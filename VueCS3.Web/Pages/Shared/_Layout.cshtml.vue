e68f49a01a6eff7298deec0bc79cd27ec723a8c3
Alex Griciuc <agriciuc@outlook.com>
2018-12-09 01:21:13 +0200

    added some dependencies

--- VueCS3.Web/Pages/Shared/_Layout.cshtml.vue
+++ VueCS3.Web/Pages/Shared/_Layout.cshtml.vue
@@ -1,23 +1,26 @@
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import '@assets/css/site.css';
-import '@assets/js/site.js';
+  import { Component, Vue } from 'vue-property-decorator';
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import 'jquery/dist/jquery.slim.min.js';
+  import '@assets/js/site.js';
+  import '@assets/css/site.css';
 
-@Component({
-  template: '#app',
-})
-export default class Layout extends Vue {
-  public test: string = 'Vue is working';
-}
+
+  @Component({
+    template: '#app',
+  })
+  export default class Layout extends Vue {
+    public test: string = 'Vue is working';
+  }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
